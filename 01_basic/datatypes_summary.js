35ffc4b1839146bc03087dfde18a778c8f62913e
Gaurav Nishad <gauravkumar749289@gmail.com>
2025-03-06 18:03:47 +0000

    stack and heap memory

--- 01_basic/datatypes_summary.js
+++ 01_basic/datatypes_summary.js
@@ -1,32 +1,53 @@
-// Primitive Datatypes
+// // Primitive Datatypes
 
-// 7 types : String, Number, Boolean, Null, undefined, Symbol, BigInt
+// // 7 types : String, Number, Boolean, Null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTem = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTem = null
+// let userEmail;
 
-const id = symbol('123')
-const anotherId = Symbol('123')
+// const id = symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id === anotherId);
-// const bigNumber = 3456784567765458n
+// console.log(id === anotherId);
+// // const bigNumber = 3456784567765458n
 
-//Reference (Non primitive)
+// //Reference (Non primitive)
 
-// Array, object, Function
+// // Array, object, Function
 
-const heros = ["shaktiman", "naagraj", "doga"];
-let myObj = {
-    name: "hitesh",
-    age: 22,
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name: "hitesh",
+//     age: 22,
+// }
+
+// const myFunction = function(){
+//     console.log("hello world");
+// }
+
+// console.log(typeof bigNumber);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+let myYoutubename = "hiteshchoudharydotcom"
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename)
+console.log(anothername)
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
 }
 
-const myFunction = function(){
-    console.log("hello world");
-}
+let userTwo = userOne
 
-console.log(typeof bigNumber);
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
